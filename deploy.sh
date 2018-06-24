c3b76afd37bbc6994bf2b0350a9007750dbfe90c
benhurdavies <benhurdavies@gmail.com>
2018-06-24 18:50:29 +0530

    update favicon and delpoy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "prod" ]; then
+  git config --global user.name "benhurdavies"
+  git config --global user.email "benhurdavies@gmail.com"
+  git remote rm origin
+  git remote add origin https://benhurdavies:${GITHUB_TOKEN}@github.com/benhurdavies/react-snake-game.git
+  npm run deploy
+fi
