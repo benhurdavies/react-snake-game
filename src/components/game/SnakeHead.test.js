06c0360cec82fe7663252d6b282bd50f45fda745
Benhur Davies <benhurdavies@gmail.com>
2018-10-08 09:33:05 +0530

    added snapshot test to snakeHead

--- src/components/game/SnakeHead.test.js
+++ src/components/game/SnakeHead.test.js
@@ -1,18 +1,25 @@
 import { shallow } from 'enzyme';
 import React from 'react';
+import renderer from 'react-test-renderer';
 
 import SnakeHead from './SnakeHead';
 
 describe('Component: SnakeHead', () => {
-  it('basic shape', () => {
-    let props = {
-      x: 10,
-      y: 10,
-      blockSize: 20,
-      theme: { snakeColor: 'black', snakeColorInvert: 'white' },
-      towards: 'left',
-    };
+  let props = {
+    x: 10,
+    y: 10,
+    blockSize: 20,
+    theme: { snakeColor: 'black', snakeColorInvert: 'white' },
+    towards: 'left',
+  };
+
+  it('basic shape check', () => {
     const wrapper = shallow(<SnakeHead {...props} />);
     expect(wrapper.find('Shape').length).toEqual(1);
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer.create(<SnakeHead {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
